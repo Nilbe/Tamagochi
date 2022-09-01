eebe7c70e45907d7c60a4962d77fd347f699f4cb
Nilbe <90683548+Nilbe@users.noreply.github.com>
2022-09-01 10:29:24 +0200

    commit 1

---
+++ Tamagochi/Tamagotchi.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public class Tamagotchi
+{
+    public string name = "Tama";
+    private int hunger = 10;
+    private int boredom = 0;
+    private bool isAlive;
+    private Random generator = new Random
+}
